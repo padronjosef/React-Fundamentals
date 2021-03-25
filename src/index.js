cb032e38ffba77346cb50d0d99ec43b077166b6f
Jose Padron <josepadron.go@gmail.com>
2021-03-24 23:25:48 -0500

    separing logic per components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './app'
 
-const FruidCard = (props) => (
-  <div>
-    <h2> {props.name} </h2>
-    <hr />
-    <p> $ {props.price} </p>
-  </div>
-)
-
-const App = () => (
-  <div>
-    <FruidCard name={'waternmelon'} price={2.00}/>
-    <FruidCard name={'pinaple'} price={5.00}/>
-    <FruidCard name={'apple'} price={11.10}/>
-    <FruidCard name={'perl'} price={2.30}/>
-    <FruidCard name={'tomato'} price={3.50}/>
-  </div>
-)
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
