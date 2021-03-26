c0b6fa3f1c9928e844615195530764a27dba26ee
Jose Padron <josepadron.go@gmail.com>
2021-03-26 01:01:39 -0500

    adding styles to fruitCard

--- src/components/FruitCard.jsx
+++ src/components/FruitCard.jsx
@@ -56,15 +56,28 @@ class FruitCard extends React.Component {
   reset = () => this.setState({ quantity: 0 });
 
   render() {
+    const hasItems = this.state.quantity > 0 
+    const styles = {
+      border: '1px solid black',
+      marginBottom: '1em',
+      borderRadius: '.5em',
+      padding: '1em',
+      background: hasItems ? 'linear-gradient(45deg, black, #4a02f7)' : 'white',
+      color: hasItems > 0 ? 'white' : 'black',
+      transition: 'all 400ms ease-out'
+    }
     return (
-      <div>
+      <div style={ styles }>
         <h2> {this.props.name} </h2>
         <h3> Quantity: {this.state.quantity} </h3>
         <button onClick={this.addOne}> + </button>
         <button onClick={this.reset}> reset </button>
         <button onClick={this.takeOne}> - </button>
-        <p> $ {this.props.price} </p>
         <hr />
+        <p> $ {this.props.price} </p>
+        <p>
+          total: ${this.props.price * this.state.quantity}
+        </p>
       </div>
     );
   }
