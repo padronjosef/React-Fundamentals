c7cf6ca023290a0a0304791945e8a5ea0890af34
Jose Padron <josepadron.go@gmail.com>
2021-03-29 23:56:30 -0500

    adding counter components and improving folders

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import FruitCard from './components/FruitCard'
+import FruitCard from './components/FruitCard/'
+import Counter from './components/Counter/'
 
 const App = () => (
   <div>
+    <Counter />
     <FruitCard name={"Watermelon"} price={2.0} />
     <FruitCard name={"Pineapple"} price={5.0} />
     <FruitCard name={"Apple"} price={11.1} />
--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import ReactDOM from "react-dom";
 import App from './app'
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// var lista1 = ['a', 'b', 'c']
+// var lista2 = [10, 35, 78]
+
+// function create_json(lista1, lista2){
+//   var resp = {}
+//   for(var i=0; i<lista1.length; i++){
+//     resp[lista1[i]] = lista2[i]
+//   }
+//   console.log(resp)
+// }
+
+// create_json(lista1, lista2)
